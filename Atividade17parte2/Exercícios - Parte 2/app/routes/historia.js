8e0623f3dbe920af8f72edb90955b3233dc008bf
GuilhermeMachadoVieira <111013975+GuilhermeMachadoVieira@users.noreply.github.com>
2024-06-06 13:00:26 -0300

    Delete Atividade17parte2/Exercícios - Parte 2 directory

--- Atividade17parte2/Exercícios - Parte 2/app/routes/historia.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app){
-    app.get('/informacao/historia', function (req, res) {
-        res.render("informacao/historia");
-        //res.end("<html><body>Historia da Fatec Sorocaba</body></html>");
-    });
-}
